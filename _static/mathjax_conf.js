903de153c07c7978f1591023df8b9d64e860d129
Nathan A. Wukie <nwukie@gmail.com>
2016-09-20 21:34:41 -0400

    Site update

---
+++ _static/mathjax_conf.js
@@ -0,0 +1,8 @@
+// Solution for improving MathJax asthetics from  http://paugargallo.com/posts/2016-05-05-sphinx-mathjax.html 
+
+MathJax.Hub.Config({
+    "HTML-CSS": {
+        availableFonts: ["TeX"],
+        scale: 90
+    }
+});

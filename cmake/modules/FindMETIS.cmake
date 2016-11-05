a1b83275b081d4ebb0faaa3d49a645a66e92d8b2
Nathan A. Wukie <nwukie@gmail.com>
2016-11-05 14:30:36 -0400

    Updated CMake build

--- cmake/modules/FindMETIS.cmake
+++ cmake/modules/FindMETIS.cmake
@@ -4,26 +4,29 @@
 #
 #
 # Typical usage:
-#   find_package (Tecplot REQUIRED)
-#   include_directories ("${TECPLOT_INCLUDE_DIR}")
+#   find_package (METIS REQUIRED)
+#   include_directories (${METIS_INCLUDE_DIR})
 #
 #
 
 
+# Set path hints from environment variable, METIS_ROOT if it is set
+set(METIS_HINT_ROOT $ENV{METIS_ROOT})
 
-# Set path hints from environment variables if they are set
-set(METIS_HINT_INCLUDE_DIR $ENV{METIS_INCLUDE_DIR})
+
+# DEPRECATED APPROACH: Set path hints from environment variables if they are set
 set(METIS_HINT_LIBRARY_DIR $ENV{METIS_LIBRARY_DIR})
+set(METIS_HINT_INCLUDE_DIR $ENV{METIS_INCLUDE_DIR})
 
 
 # Find include file and library
 find_path(METIS_INCLUDE_DIR 
           metis.h 
-          HINTS ${METIS_HINT_INCLUDE_DIR})
+          HINTS ${METIS_HINT_ROOT} ${METIS_HINT_INCLUDE_DIR})
 
 find_library(METIS_LIBRARIES 
              metis 
-             HINTS ${METIS_HINT_LIBRARY_DIR})
+             HINTS ${METIS_HINT_ROOT} ${METIS_HINT_LIBRARY_DIR})
 
 # Set METIS_FOUND if conditions are met
 if(METIS_INCLUDE_DIR)

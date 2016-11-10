a86bb6227b63745537bd488365b3aceccb406905
Mayank Sharma <mayanksharma1806@gmail.com>
2016-11-10 12:29:45 -0500

    vtk utility: hdf2vtk file, updated chidg post and makefile

--- src/actions/post/chidg_post.f90
+++ src/actions/post/chidg_post.f90
@@ -1,5 +1,7 @@
 module mod_chidg_post
     use mod_chidg_post_hdf2tec, only: chidg_post_hdf2tec
+    use mod_chidg_post_hdf2vtk, only: chidg_post_hdf2vtk
+
     implicit none
 
 
@@ -24,6 +26,40 @@ contains
 
     end subroutine chidg_post
     !**************************************************************************
+
+
+
+    
+    !>  Interface for file conversion.
+    !!
+    !!  @author Mayank Sharma
+    !!  @date   11/06/2016
+    !!
+    !!
+    !--------------------------------------------------------------------------
+    subroutine chidg_post_vtk(filename)
+        character(*),   intent(in)  :: filename
+
+
+        call chidg_post_hdf2vtk(filename)
+
+
+    end subroutine chidg_post_vtk
+    !**************************************************************************
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

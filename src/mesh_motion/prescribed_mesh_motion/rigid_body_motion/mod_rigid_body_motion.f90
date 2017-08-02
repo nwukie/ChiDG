59acbb37b6ab171c66665177c56bf7f1639a7a64
Eric M. Wolf <wolfem@sauron.rz.afrl-wrs.hpc.mil>
2017-08-02 12:06:18 -0400

    Fixed issue with the initial values for RB PMMF parameters.

--- src/mesh_motion/prescribed_mesh_motion/rigid_body_motion/mod_rigid_body_motion.f90
+++ src/mesh_motion/prescribed_mesh_motion/rigid_body_motion/mod_rigid_body_motion.f90
@@ -1,10 +1,10 @@
 module mod_rigid_body_motion
     use mod_kinds,      only: rk, ik
-    use mod_constants,  only: ZERO
+    use mod_constants,  only: ZERO, ONE
     implicit none
 
     real(rk) :: rigid_body_t0 = ZERO
-    real(rk) :: rigid_body_t1 = ZERO
+    real(rk) :: rigid_body_t1 = ONE 
     real(rk) :: rigid_body_motion_disp_old(3) = ZERO
     real(rk) :: rigid_body_motion_disp_new(3) = ZERO
     real(rk) :: rigid_body_motion_vel(3) = ZERO

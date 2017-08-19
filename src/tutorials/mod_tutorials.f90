95ce322860d6d9ea07708c3ae4afdaddd73a40f1
Nathan A. Wukie <nwukie@gmail.com>
2017-08-19 14:39:15 -0400

    Commiting some files from 'dev' branch

--- src/tutorials/mod_tutorials.f90
+++ src/tutorials/mod_tutorials.f90
@@ -1,7 +1,7 @@
 module mod_tutorials
 #include <messenger.h>
-    use mod_tutorial_smoothbump,    only: tutorial_driver_smoothbump
-    use mod_tutorial_smoothbump_new,    only: smooth_bump_tutorial_t
+!    use mod_tutorial_smoothbump,    only: tutorial_driver_smoothbump
+!    use mod_tutorial_smoothbump_new,    only: smooth_bump_tutorial_t
     implicit none
 
 
@@ -20,19 +20,19 @@ contains
     subroutine tutorial_driver(tutorial)
         character(*),   intent(in)  :: tutorial
 
-        type(smooth_bump_tutorial_t)    :: smooth_bump_tutorial
-
-
-        select case (trim(tutorial))
-            case ('smooth_bump')
-                !call tutorial_driver_smoothbump()
-                call smooth_bump_tutorial%initialize()
-                call smooth_bump_tutorial%run()
-
-            case default
-                call chidg_signal(FATAL,"tutorial_driver: the available tutorials are 'smooth_bump'.")
-
-        end select
+!        type(smooth_bump_tutorial_t)    :: smooth_bump_tutorial
+!
+!
+!        select case (trim(tutorial))
+!            case ('smooth_bump')
+!                !call tutorial_driver_smoothbump()
+!                call smooth_bump_tutorial%initialize()
+!                call smooth_bump_tutorial%run()
+!
+!            case default
+!                call chidg_signal(FATAL,"tutorial_driver: the available tutorials are 'smooth_bump'.")
+!
+!        end select
 
 
     end subroutine tutorial_driver
